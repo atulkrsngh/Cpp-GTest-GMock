5458f6602aa70cf4e05a027136cef3b84865148f
Atul Kumar <33157076+atulkrsngh@users.noreply.github.com>
2024-08-23 16:43:48 +0530

    Update test_mock.cpp

--- cpp_test/test_mock.cpp
+++ cpp_test/test_mock.cpp
@@ -62,3 +62,23 @@ TEST(myDBTest, logintest) {
     int retValue = db.init("Terminator", "I will be back"); // Act
     EXPECT_EQ(retValue, 1); // Assert
 }
+
+/*
+
+Flexibility: ON_CALL gives you flexibility to set up default behaviors that help the function under test execute 
+correctly, even if those behaviors are not the primary focus of the test.
+
+Non-Strict Behavior: ON_CALL does not impose strict requirements on method calls (like number of times or specific 
+arguments). This means you can use it to provide supportive behavior without making the test dependent on exact calls.
+
+Supplementary Actions: Use ON_CALL when you want to do something extra or need a generic setup that isn't directly 
+tested but supports the overall testing environment.
+
+EXPECT_CALL is used when you need to verify specific behaviors, enforce certain conditions, or ensure that a method 
+is called a specific number of times with specific arguments. It’s stricter and is used to check interactions with 
+the mock object directly as part of the test's assertions.
+
+Use ON_CALL to set up default behaviours or perform supplementary actions that aren't the main focus of your test.
+Use EXPECT_CALL to assert specific behaviors, interactions, or conditions that are central to what you are testing.
+
+*/
